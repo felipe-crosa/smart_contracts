e38e05f6b44d07e87972120443240a9af2ed8009
Felipe Crosa <felicrosa@gmail.com>
2024-04-21 18:56:04 +0200

    Arreglar contrato IERC721

--- contratos/src/interfaces/IERC20.sol
+++ contratos/src/interfaces/IERC20.sol
@@ -1,6 +1,5 @@
 pragma solidity 0.8.19;
 
-/// @dev NFT interface
 interface IERC20 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
