b46501185f5825031681d5e953e08c701d5075aa
Felipe Crosa <felicrosa@gmail.com>
2024-04-21 19:16:35 +0200

    Definir interfaz para el contrato de Sale

---
+++ contratos/src/interfaces/ISale.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.19;
+
+interface ISale {
+    enum Status { ACCEPTED, REJECTED, WAITING, CANCELED}
+
+    struct SaleMetadata {
+        address offeringAddress;
+        uint256 wantedTokenId;
+        uint256 amount;
+        Status status;
+    }
+
+    function publishOffer(uint256 _wantedTokenId, uint256 _amount) external returns (uint256 _offerId);
+    function rejectOffer(uint256 _offerId) external;
+    function acceptOffer(uint256 _offerId) external;
+    function cancelOffer(uint256 _offerId) external;
+}
+    
